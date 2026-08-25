8351152400a5c811a0507bd8344dbe1068c4c34d
hasib116 <hasibul.017bd@gmail.com>
2026-08-26 00:57:58 +0600

    index update

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) session_start();
 
-// If already logged in, send straight to their dashboard
 if (isset($_SESSION['role'])) {
     header("Location: /smartfarm/" . $_SESSION['role'] . "/dashboard.php");
     exit;
 }
+
 
 $pageTitle = "Welcome";
 require_once __DIR__ . '/includes/header.php';
