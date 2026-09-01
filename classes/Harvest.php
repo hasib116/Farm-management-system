6fcda87497d84fbb68789bf06417a48a62a967e6
khannoor90000-crypto <khannoor90000@gmail.com>
2026-09-01 21:18:11 +0600

    Crop as Harvested Update

--- classes/Harvest.php
+++ classes/Harvest.php
@@ -20,7 +20,6 @@ class Harvest
         $ok = $stmt->execute();
 
         if ($ok) {
-            // mark the crop as harvested
             $cropStmt = $this->conn->prepare("UPDATE crops SET status = 'harvested' WHERE id = ?");
             $cropStmt->bind_param("i", $crop_id);
             $cropStmt->execute();
